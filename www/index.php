67ed87b5a8c1b4de664d4035b2f513938818a9a1
Andres Garcia <desarrollo02@cebsa.mx>
2022-05-27 12:26:52 -0500

    agrego funciones crear_tabla() y insertar_filas()

--- www/index.php
+++ www/index.php
@@ -49,61 +49,70 @@
     // show tables (prefiero esta porque solo me trae las tablas de la bd actaul)
     // select table_name from information_schema.tables;
 
-    // apartir de aqui crea un funcion para insertar datos, pasandole de parametros el nombre de la tabla y el limit
-    $usuarios = $pdo_dos ->query("SELECT * 
-    FROM usuarios 
-    where codNoEmpleado not in (1,2)")->fetchAll(PDO::FETCH_ASSOC);
 
+    $insertar_sql = crear_tabla($pdo, $pdo_dos, "usuarios");
+    insertar_filas($pdo, $pdo_dos, "usuarios", 1000, $insertar_sql);
 
-    $describe = $pdo_dos ->query("describe usuarios")->fetchAll(PDO::FETCH_ASSOC);
-    $crear_table = "CREATE TABLE usuarios (";
-    $inserta_sql = "INSERT INTO usuarios (";
-    $index_i = 0;
-    $describe_lenght = count($describe);
-    foreach($describe as $i => $fila){
-        if($index_i < $describe_lenght-1){
-            $crear_table.= $fila["Field"]. " " . $fila["Type"]. " DEFAULT NULL, ";
-            $inserta_sql.=$fila["Field"].",";
-        }else{
-            $crear_table.= $fila["Field"]. " " . $fila["Type"]. " DEFAULT NULL ";
-            $inserta_sql.=$fila["Field"];
-        }
-        $index_i+=1;
-    }
-    $inserta_sql .= ") VALUES ";
-    $crear_table.=")";
-    // echo $crear_table;
-    $pdo->exec($crear_table);
-
-    
-    $inserta_usuario = "";
-    $temp_dos = count($usuarios);
-    $temp = count($usuarios[0]);
-    $index_i = 0;
-    foreach($usuarios as $i =>$usuario){
-        $inserta_usuario.= "(";
-        $index_j = 0;
-        foreach($usuario as $j => $col){  
-            if($col==""){
-                $col = 0;
-            }
-            if($index_j < $temp -1){
-                $inserta_usuario.="'".$col."',";
+    function crear_tabla($pdo, $pdo_origen, $nombre_tabla){
+        $describe = $pdo_origen ->query("describe $nombre_tabla")->fetchAll(PDO::FETCH_ASSOC);
+        $crear_table = "CREATE TABLE $nombre_tabla (";
+        $inserta_sql = "INSERT INTO $nombre_tabla (";
+        $index_i = 0;
+        $describe_lenght = count($describe);
+        foreach($describe as $i => $fila){
+            if($index_i < $describe_lenght-1){
+                $crear_table.= $fila["Field"]. " " . $fila["Type"]. " DEFAULT NULL, ";
+                $inserta_sql.=$fila["Field"].",";
             }else{
-                $inserta_usuario.="'".$col."'";
+                $crear_table.= $fila["Field"]. " " . $fila["Type"]. " DEFAULT NULL ";
+                $inserta_sql.=$fila["Field"];
             }
-            $index_j+= 1;
+            $index_i+=1;
         }
-        if($index_i < $temp_dos -1){
-            $inserta_usuario.="), ";
-        }else{
-            $inserta_usuario.=");";
-        }
-        $index_i+= 1;
+        $inserta_sql .= ") VALUES ";
+        $crear_table.=")";
+        // echo $crear_table;
+        $pdo->exec($crear_table);
+        return $inserta_sql;
     }
-    $inserta_sql.= $inserta_usuario;
-    // echo $inserta_sql; 
-    $pdo->exec($inserta_sql);
+
+    function insertar_filas($pdo, $pdo_origen, $nombre_tabla, $limite, $inserta_sql){
+        $filas = $pdo_origen ->query("SELECT * 
+        FROM $nombre_tabla 
+        where codNoEmpleado not in (1,2)
+        limit $limite")->fetchAll(PDO::FETCH_ASSOC);
+
+
+        $inserta_usuario = "";
+        $temp_dos = count($filas);
+        $temp = count($filas[0]);
+        $index_i = 0;
+        foreach($filas as $i =>$usuario){
+            $inserta_usuario.= "(";
+            $index_j = 0;
+            foreach($usuario as $j => $col){  
+                if($col==""){
+                    $col = 0;
+                }
+                if($index_j < $temp -1){
+                    $inserta_usuario.="'".$col."',";
+                }else{
+                    $inserta_usuario.="'".$col."'";
+                }
+                $index_j+= 1;
+            }
+            if($index_i < $temp_dos -1){
+                $inserta_usuario.="), ";
+            }else{
+                $inserta_usuario.=");";
+            }
+            $index_i+= 1;
+        }
+        $inserta_sql.= $inserta_usuario;
+        // echo $inserta_sql; 
+        $pdo->exec($inserta_sql);
+    }
+
     ?>
 
     
