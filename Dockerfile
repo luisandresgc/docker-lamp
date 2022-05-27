c12c3436557c6e265415183f793dbfd4413ffe7e
Andres Garcia <desarrollo02@cebsa.mx>
2022-05-26 22:35:24 -0500

    agrego pdo y variables de seguridad

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,4 @@ RUN apt-get update \
     && apt-get install -y zlib1g-dev \
     && rm -rf /var/lib/apt/lists/* \
     && docker-php-ext-install zip
+RUN docker-php-ext-install mysqli
--- www/index.php
+++ www/index.php
@@ -14,6 +14,8 @@
     <?php echo "<h1>Hi! I'm happy</h1>"; ?>
 
     <?php
+    require_once 'security.php';
+
 
     // Connexion et sélection de la base
     $conn = mysqli_connect('db', 'user', 'test', "myDb");
@@ -39,6 +41,42 @@
     $result->close();
 
     mysqli_close($conn);
+
+    try {
+        $pdo = new PDO("mysql:dbname=myDb;host=db;",
+                        "user",
+                        "test"
+                        , array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET NAMES utf8")
+                    );
+         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo
+       $error_message = date('Y-m-d G:i:s') . " [ERROR]: " . $e->getMessage() . "\n\r";
+       file_put_contents('PDOErrors.txt', $error_message, FILE_APPEND);
+        $db = null;
+    }
+    $temp = $pdo->query("SELECT * FROM Person")->fetch(PDO::FETCH_COLUMN);
+
+    echo "<h1>$temp</h1> "; 
+
+
+    try {
+        $pdo_dos = new PDO("mysql:dbname=$MYSQL_DATABASE;host=$MYSQL_HOST;",
+                        "$MYSQL_USER",
+                        "$MYSQL_PASSWORD"
+                        , array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET NAMES utf8")
+                    );
+         $pdo_dos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo
+       $error_message = date('Y-m-d G:i:s') . " [ERROR]: " . $e->getMessage() . "\n\r";
+       file_put_contents('PDOErrors.txt', $error_message, FILE_APPEND);
+        $db = null;
+    }
+
+    $temp = $pdo_dos->query("SELECT sucursal FROM usuarios limit 1")->fetch(PDO::FETCH_COLUMN);
+
+    echo "<h1>$temp</h1> "; 
 
     ?>
     </div>
